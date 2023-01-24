9f1d0ad75c6983d06477da793ef2db8be2ceca72
Francisco Freire <franciscocfreire@gmail.com>
2023-01-24 18:29:17 -0300

    Add Chainlink VRFv2

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,40 +8,51 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoghETHEntered();
 
-contract Raffle{
+contract Raffle is VRFConsumerBaseV2{
 
-    /* State variables */abi
+    /* State variables */
     uint256 private immutable i_entraceFee;
     address payable[] private s_players;
 
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee){
+    constructor(address vrfCoordinatorv2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorv2){
         i_entraceFee = entranceFee;
     }
 
     function enterRaffle() public payable {
         // require msg.value > i_entranceFee, "not enough ETH"
-        if(msg.value <= entranceFee){
+        if(msg.value <= i_entraceFee){
             revert Raffle__NotEnoghETHEntered();
         }
 
-        s_players.push(payable(msg.sender))
-        emit RaffleEnter(msg.sender)
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
         //Emit an event when we update a dynamic 
 
     }
 
-    // function pickRadomWinner(){}
+    function requestRadomWinner() external {
+        // Request the random number
+        // Once we gert it, do something with it
+        // 2 transaction process
+    }
 
-    function getEntranceFee() public view returns(int256){
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) 
+    internal override {
+
+    }
+
+    function getEntranceFee() public view returns(uint256){
         return i_entraceFee;
     }
 
-    function getPlayer(uint256 index) public biew returns(address){
-        return s_players[index]
+    function getPlayer(uint256 index) public view returns(address){
+        return s_players[index];
     }
 }
